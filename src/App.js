42141014d35b72f335cf00718506a761f930488e
root <root@debian.jose>
2018-10-01 12:20:54 -0500

    conteo

--- src/App.js
+++ src/App.js
@@ -1,107 +1,90 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-/*import Navigation from './components/Navigation';*/
+
+// data
+import { todos } from './todos.json';
+
+// subcomponents
 import TodoForm from './components/TodoForm';
-import {todos} from './todos.json';
-console.log(todos);
 
 class App extends Component {
-  constructor (){
+  constructor() {
     super();
-    this.state={
+    this.state = {
       todos
-     
-    };
-    this.handleAddTodo=this.handleAddTodo.bind(this);
-
+    }
+    this.handleAddTodo = this.handleAddTodo.bind(this);
   }
 
-handleAddTodo(todo){
+  removeTodo(index) {
+    this.setState({
+      todos: this.state.todos.filter((e, i) => {
+        return i !== index
+      })
+    });
+  }
+
+  handleAddTodo(todo) {
     this.setState({
       todos: [...this.state.todos, todo]
     })
   }
 
-removeTodo(index){
-  console.log(index);
-  if (window.confirm('ESTAS SEGURO DE  QUERERLO ELIMINARLO')){
-
-    this.setState({
-    todos: this.state.todos.filter((e, i) =>{
-      return i !== index
-    })
-  })
-  }
-}
-  
-render() {
-    
-const todos=this.state.todos.map((todo, i) => {
-      return(
-        
-      
-        <div className='col-md-4' Key={i} >
-
-        <div className='card mt-4'>
-        <div className='card-header'>
-        <h3>{todo.title}</h3>
-        <span className="badge badge-pill badge-danger ml-2">
-          {todo.priority}
-        </span>
-
-        </div>
-        <div className='card-body'>
-          <p>{todo.description}</p>
-          <p>{todo.responsible}</p>
-        </div>
-          <div className="card-footer">
-          <button type="button" class="btn btn-primary" onClick={this.removeTodo.bind(this, i)}> Delete</button>
-
+  render() {
+    const todos = this.state.todos.map((todo, i) => {
+      return (
+        <div className="col-md-4" key={i}>
+          <div className="card mt-4">
+            <div className="card-title text-center">
+              <h3>{todo.title}</h3>
+              <span className="badge badge-pill badge-danger ml-2">
+                {todo.priority}
+              </span>
+            </div>
+            <div className="card-body">
+              {todo.description}
+            </div>
+            <div className="card-footer">
+              <button
+                className="btn btn-danger"
+                onClick={this.removeTodo.bind(this, i)}>
+                Delete
+              </button>
+            </div>
           </div>
         </div>
+      )
+    });
 
-
-        </div>
-
-
-      
-        )
-
-        })
- 
-
+    // RETURN THE COMPONENT
     return (
       <div className="App">
-       
-          
-      <nav className="navbar navbar-dark bg-dark">
-      <a href="" className="text-white">
-       Task
-       <span className="badge badge-pill badge-light ml-2">
-       {this.state.todos.lenght}
-       
 
-       </span>  
-     
-      </a>
-      </nav>
-            
-            <div className="container">
-            <div className="row mt-4">
-            {todos}
+        <nav className="navbar navbar-dark bg-dark">
+          <a className="navbar-brand" href="/">
+            Tasks
+            <span className="badge badge-pill badge-light ml-2">
+              {this.state.todos.length}
+            </span>
+          </a>
+        </nav>
+
+        <div className="container">
+          <div className="row mt-4">
+
+            <div className="col-md-4 text-center">
+                <img src={logo} className="App-logo" alt="logo" />
+              <TodoForm onAddTodo={this.handleAddTodo}></TodoForm>
             </div>
 
+            <div className="col-md-8">
+              <div className="row">
+                {todos}
+              </div>
             </div>
-          <div className="col md-5">
-          <img src={logo} className="App-logo" alt="logo" />
-         <TodoForm onAddTodo={this.handleAddTodo}/>
-         <p className="App-intro">
-           tigrezhito.
-           </p>
+          </div>
         </div>
-        
-        
       </div>
     );
   }
